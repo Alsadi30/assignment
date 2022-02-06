1f6e92b848a5dfd0d8c284d18eb9739d843df0fb
sadi <abdullahsadi39@gmail.com>
2022-02-06 19:35:27 +0600

    first commit

---
+++ conditions.js
@@ -0,0 +1,93 @@
+
+if (isRaining) {
+    return 'Do not go to outside'
+} else {
+    return 'Can go to outside'
+}
+
+
+if (wanttosleep) {
+    return 'Turn off the light'
+} else {
+    return 'Light should be on'
+}
+
+if (inClass) {
+    return 'Phone should be silent'
+} else {
+    return 'Increse ringing volume'
+}
+
+
+if (itsMorning) {
+    console.log('Take a cup of tea')
+}
+
+
+if (itsAfternoon) {
+    console.log('Take a lunch')
+}
+
+
+if (itsNight) {
+    console.log('Take a dinner')
+}
+
+if (attandInVarsityClasses) {
+    console.log('Will get full mark of attandance')
+}
+
+
+if (varsityOff) {
+    console.log('Learn Programming')
+}
+
+
+if (coldWeather) {
+    console.log('Have to Wear a Jacket')
+}
+
+
+if (fillLazy) {
+    console.log('Take a cup of tea')
+}
+
+if (itsHoliday) {
+    console.log('Go to outside')
+}
+
+if (wifiAvailable) {
+    console.log('Turn off phone data')
+}
+
+
+if (chargeofPhoneisLow) {
+    console.log('Plugin phone with charger')
+}
+
+
+if (noFoodInHome) {
+    console.log('Go to Restrurent')
+}
+
+
+if (wantToplayfootball) {
+    gotofield()
+}
+
+
+if (assignmentDone) {
+   CanOnetoOneConversetion()
+}
+
+if (HasTime) {
+    playGame()
+}
+
+if (WantToLoseWight) {
+    Exercise()
+}
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,19 @@
+
+
+// 20 real-life examples that we can relate with loops in programming
+
+// To send invitation mail to 50 people.
+// To give blanket to 20 poor people.
+// Serve food to people.
+// To give medels to winners.
+// To take attandance of a class students.
+// In a bank queue .
+// In shoping mall's counter queue.
+// Distribute money to people.
+// Give NID card.
+// In Vote center.
+// In ticket conter of bus/train.
+// In airport.
+// To sort out goods from a heap
+// For lebeling / add tag in shoping items.
+// Delivering same type of materials in different location.
